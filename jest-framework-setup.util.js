f3e56e183a29bc43de81713d53e51f2cc18ba94f
bguiz <github@bguiz.com>
2017-08-01 22:57:52 +1000

    =BG= feat: property test utils for test check refactored
    - extracted to jest-object

--- jest-framework-setup.util.js
+++ jest-framework-setup.util.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('jasmine-check').install();
-
 /* istanbul ignore next */
 afterAll((done) => {
 	//NOTE call done callback every time a file has finished being tested
--- math.ppty.jest.js
+++ math.ppty.jest.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const testcheck = require('testcheck');
-
-const testUtil = require('./test.util.js');
+const pptyTestUtil = require('jest-object/property-test-util-testcheck.js');
 
 const math = require('./math.js');
 
@@ -21,40 +20,7 @@ describe('[math.sum]', () => {
 			return result;
 		}
 
-		const spec =
-		it('with new', () => {
-			// Always run with a new seed
-			const specSeedsInfo = testUtil.getJestSpecSeedsInfo(spec, __filename);
-			const result = commutativeCheckProperty({
-			});
-			if (!result.result) {
-				// This has failed, and we will save the seed so that we an repeat them later
-				const newSeed = {
-					seed: result.seed,
-				}
-				specSeedsInfo.snapshotSeeds.push(newSeed);
-				// Add a prior seeds snapshot
-				expect(result).toMatchSnapshot(`${descSpec.result.fullName} ${result.seed}`)
-			}
-			// This has the effect of appending the failure seed
-			// If there is a new failure, the snapshot will not match, and thus fail the test
-			expect(specSeedsInfo.snapshotSeeds).toMatchSnapshot(specSeedsInfo.snapshotSeedsName);
-		});
-
-		const descSpec =
-		describe('[prior seeds]', () => {
-			const specSeedsInfo = testUtil.getJestSpecSeedsInfo(spec, __filename);
-			specSeedsInfo.snapshotSeeds.forEach((info) => {
-				const seed = info.seed;
-				it(`${seed}`, () => {
-					// Always run with a seed that has previously failed
-					const result = commutativeCheckProperty({
-						seed,
-					});
-					expect(result).toMatchSnapshot();
-				});
-			});
-		});
+		pptyTestUtil.seededPropertyTestFactory(commutativeCheckProperty, __filename);
 
 	});
 
